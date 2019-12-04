0b83c84606aac220fe9b72bfb423c1be22abdee7
sienaiwun <sienaiwun@gmail.com>
2019-12-04 10:43:15 +0800

    Fix additional light shadowmask

--- com.unity.render-pipelines.universal/ShaderLibrary/Shadows.hlsl
+++ com.unity.render-pipelines.universal/ShaderLibrary/Shadows.hlsl
@@ -229,16 +229,16 @@ half MainLightRealtimeShadow(float4 shadowCoord)
 
 half AdditionalLightRealtimeShadow(int lightIndex, float3 positionWS, float4 shadowmask)
 {
-#if !defined(ADDITIONAL_LIGHT_CALCULATE_SHADOWS)
-    return 1.0h;
-#endif
-
 #if defined(SHADOWS_SHADOWMASK) && defined(LIGHTMAP_ON)
     int chanel = _AdditionalLightsSpotDir[lightIndex].w;
     if (chanel >= 1 && chanel <= 4)
          return shadowmask[chanel - 1];
     else
         return 1.0f;
+#endif
+
+#if !defined(ADDITIONAL_LIGHT_CALCULATE_SHADOWS)
+    return 1.0h;
 #endif
 
     ShadowSamplingData shadowSamplingData = GetAdditionalLightShadowSamplingData();
